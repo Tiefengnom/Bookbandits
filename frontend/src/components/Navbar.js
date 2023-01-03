b1c105c0460af3122d71199904783b42fba3e7b2
Roxana Bita <roxana.bita@gmail.com>
2023-01-03 17:36:07 +0100

    link in navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,21 @@
-import { Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 //
 
 const Navbar = () => {
+	return (
+		<header>
+			<div className='container'>
+				<Link to='/'>
+					<h1>BookBandits</h1>
+				</Link>
+				<ul>
+					<Link to='/:userid/addbook'>
+						<li>Add Book</li>
+					</Link>
+				</ul>
+			</div>
+		</header>
+	);
+};
 
-  return (
-   <header>
-    <div className = "container">
-        <Link to= "/">
-            <h1>BookBandits</h1>
-        </Link>
-    </div>
-</header>
-)
-} 
-
-export default Navbar 
+export default Navbar;
