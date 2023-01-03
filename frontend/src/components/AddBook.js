63936481167535ace397af1d9083a986b7ca4d9c
Roxana Bita <roxana.bita@gmail.com>
2023-01-03 15:59:01 +0100

    display book info

--- frontend/src/components/AddBook.js
+++ frontend/src/components/AddBook.js
@@ -4,9 +4,8 @@ import axios from "axios";
 function AddBook() {
 	const [input, setInput] = useState("");
 	const [books, setBooks] = useState([]);
-	const [searchKey, setSearchKey] = useState("");
-	// let value='978-9734673223';
-	// let url=`https://www.googleapis.com/books/v1/volumes?q=isbn:0883454726&key=AIzaSyAxUzNiUiHcisMxQTYFq5Hy-35A2C91-fQ`;
+	const [searchKey, setSearchKey] = useState();
+
 
 	const handleChange = ({ target }) => {
 		setInput(target.value);
@@ -21,6 +20,7 @@ function AddBook() {
 	const getBooks = async () => {
 		console.log(searchKey);
     // %3A is url-encoding of char ':'
+    
     const url = `https://www.googleapis.com/books/v1/volumes?q=isbn%3A${searchKey}&key=AIzaSyAxUzNiUiHcisMxQTYFq5Hy-35A2C91-fQ`; 
     console.log(url);
 
@@ -41,19 +41,27 @@ function AddBook() {
 				onSubmit={handleSubmit}>
 				<label>Enter ISBN</label>
 				<input
-					placeholder='enter isbn'
+				
 					value={input}
 					onChange={handleChange}></input>
 				<button type='submit'>Search </button>
 			</form>
+      
 			<div className='search-result'>
-				{books
+				{ searchKey ? (
+        books
 					? books.map((b) => {
 							return (
-								<p>{b.volumeInfo.title}</p>
+                <div>
+								<p><strong>Title:</strong> {b.volumeInfo.title}</p>
+                <p><strong>Authors:</strong> {b.volumeInfo.authors}</p>
+                <p><strong>Categories:</strong> {b.volumeInfo.categories}</p>
+                <p><strong>Language:</strong> {b.volumeInfo.language}</p>
+                <p><strong>Description:</strong> {b.volumeInfo.description}</p>
+                </div>
 							);
 					  })
-					: "loading info"}
+					: "loading info" ) : "Please enter search key"}
 			</div>
 		</div>
 	);
