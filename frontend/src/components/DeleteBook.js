5602018fc72c9ab1b365aeb5f0d80654b04d24e2
Roxana Bita <roxana.bita@gmail.com>
2023-01-07 16:41:53 +0100

    create files for components

--- frontend/src/components/DeleteBook.js
+++ frontend/src/components/DeleteBook.js
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
 
 const DeleteBook = ({ id }) => {
-	const [error, setError] = useState(null);
+    const [error, setError] = useState(null);
 
-	const deleteBook = async () => {
-		const url = `http://localhost:4000/bookbandits/user/user_collection/${id}`;
+    const deleteBook = async () => {
+        const url = `http://localhost:4000/bookbandits/user/user_collection/${id}`;
 
-		const response = await fetch(url, {
-			method: "DELETE",
-		});
-		const json = await response.json();
+        const response = await fetch(url, {
+            method: "DELETE",
+        });
+        const json = await response.json();
 
-		if (response.ok) {
-			setError(null);
-			console.log("book deleted", json);
-		} else {
-			setError(json.error);
-		}
-	};
+        if (response.ok) {
+            setError(null);
+            console.log("book deleted", json);
+        } else {
+            setError(json.error);
+        }
+    };
 
-	return (
-		<div>
-			<button onClick={deleteBook}>Delete</button>
-		</div>
-	);
+    return (
+        <div>
+            <button onClick={deleteBook}>Delete</button>
+        </div>
+    );
 };
 
 export default DeleteBook;
