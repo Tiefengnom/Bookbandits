8836451c06d52ecd999501e2296f511d5189f36d
Roxana Bita <roxana.bita@gmail.com>
2023-01-07 16:38:35 +0100

    delete button works

--- frontend/src/components/DeleteBook.js
+++ frontend/src/components/DeleteBook.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const DeleteBook = ({id}) => {
-	const deleteBook = () => {
-    console.log(`TODO: delete book ${id} from db`);
-  };
+const DeleteBook = ({ id }) => {
+	const [error, setError] = useState(null);
+
+	const deleteBook = async () => {
+		const url = `http://localhost:4000/bookbandits/user/user_collection/${id}`;
+
+		const response = await fetch(url, {
+			method: "DELETE",
+		});
+		const json = await response.json();
+
+		if (response.ok) {
+			setError(null);
+			console.log("book deleted", json);
+		} else {
+			setError(json.error);
+		}
+	};
 
 	return (
 		<div>
 			<button onClick={deleteBook}>Delete</button>
 		</div>
 	);
-}
+};
 
 export default DeleteBook;
