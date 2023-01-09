d263c2bcda873f5ea324a052af299f6a5c4304b9
Roxana Bita <roxana.bita@gmail.com>
2023-01-09 23:32:58 +0100

    responsive navbar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Catalogue from "./Catalogue";
-
+import '../index.css'
 //right now catalogue is a component in home and a stand-alone page..need to decide
 
 const Home = () => {
     return (
   
         <div className='header h-screen w-full bg-gradient-to-br from-yellow-500 to-pink-600 pt-12 pb-12 px-4 text-white'>
-            <h1 className='w-full text-center text-4xl p-3 h-24'>Free your Books. Be a BookBandit.</h1>
+            <h1 className='hero-text w-full text-center text-4xl p-3 h-24 mt-[100px]'>Free your Books. Be a BookBandit.</h1>
             <div className='home search flex justify-center '>
                 <Catalogue />
             </div>
