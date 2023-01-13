dd474f99d30b2066a95d573c97e9810d46ee5977
Roxana Bita <roxana.bita@gmail.com>
2023-01-13 11:04:19 +0100

    home buttons changed to catalog

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -16,22 +16,22 @@ const Home = () => {
                 </h1>
 
                 <button
-                    onClick={() => navigate("/login")}
-                    className='m-3 inline-block px-6 py-2 border-2 border-white-500  font-medium text-xs leading-tight uppercase rounded-full hover:bg-pink-600 hover:bg-opacity-[45%] focus:outline-none focus:ring-0 transition duration-150 ease-in-out cursor:pointer'>
-                    Login
+                    onClick={() => navigate("/catalogue")}
+                    className='m-3  bg-white bg-opacity-60 inline-block px-6 py-2 border-2 border-white-500  font-medium text-xs leading-tight uppercase rounded-full hover:bg-pink-600 hover:bg-opacity-[45%] focus:outline-none focus:ring-0 transition duration-150 ease-in-out cursor:pointer'>
+                    Browse
                 </button>
 
                 <button
                     onClick={() => navigate("/signup")}
-                    className='m-3 inline-block px-6 py-2 border-2 border-white-500 font-medium text-xs leading-tight uppercase rounded-full hover:bg-pink-600 hover:bg-opacity-[45%] focus:outline-none focus:ring-0 transition duration-150 ease-in-out cursor:pointer'>
+                    className='m-3 bg-white bg-opacity-60 inline-block px-6 py-2 border-2 border-white-500 font-medium text-xs leading-tight uppercase rounded-full hover:bg-pink-600 hover:bg-opacity-[35%] focus:outline-none focus:ring-0 transition duration-150 ease-in-out cursor:pointer'>
                     Sign up
                 </button>
-                <img src={girl} alt='illustration-girl' className="w-[400px]" />
+                <img src={girl} alt='illustration-girl' className="w-[400px] mt-4" />
             </div>
             <div className='right'>
                 <div className='illustrations'>
                 
-                    <img src={boy} alt='illustration-boy' className="w-[500px] hidden lg:inline" />
+                    <img src={boy} alt='illustration-boy' className="w-[500px] hidden lg:inline ml-4" />
                 </div>
             </div>
             {/* <div className='home search flex justify-center '>
