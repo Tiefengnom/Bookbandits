74bb8a13ca4ad16f58b56c0d365a3c479ea11557
Roxana Bita <roxana.bita@gmail.com>
2023-01-14 14:00:23 +0100

    fix search by genre

--- backend/controllers/bookcontroller.js
+++ backend/controllers/bookcontroller.js
@@ -96,37 +96,45 @@ const getBooks = async (req,res) => {
   
   //Search books by title, author, category and synopsis (global search)
   const searchBooks = async (req, res) => {
-   try{   
-   console.log("params", req.params);
-      console.log("body", req.body);
+      try {
+          console.log("params", req.params);
+          console.log("body", req.body);
 
-      const query = {
-          $and: [],
-      };
+          const query = {
+              $and: [],
+          };
 
-      if (req.params.query || req.body.query) {
-          const search = { $regex: req.params.query || req.body.query, $options: "i" };
-          query.$and.push({ $or: [{ title: search }, { author: search }, { category: search }, { synopsis: search }] });
-      }
+          if (req.body.query) {
+              const search = { $regex: req.body.query, $options: "i" };
+              query.$and.push({
+                  $or: [{ title: search }, { author: search }, { category: search }, { synopsis: search }],
+              });
+          }
 
-      if (req.body.language) {
-          query.$and.push({ language: req.body.language });
-      }
+          if (req.body.language) {
+              query.$and.push({ language: req.body.language });
+          }
 
-      if (req.body.availability) {
-         query.$and.push({ borrowed: req.body.availability });
-     }
-console.log(query)
-      const books = await Book.find(query, { title: 1, author: 1, synopsis: 1, category: 1 }).sort({
-          createdAt: -1,
-      });
+          if (req.body.genre) {
+              query.$and.push({ category: { $regex: req.body.genre, $options: "i" } });
+          }
 
-      res.status(200).json(books);}
-      catch (error) {
-         console.log(error)
+          console.log(query);
+          const books = await Book.find(query.$and.length ? query : {}, {
+              title: 1,
+              author: 1,
+              synopsis: 1,
+              category: 1,
+          }).sort({
+              createdAt: -1,
+          });
+
+          res.status(200).json(books);
+      } catch (error) {
+          console.log(error);
       }
   };
-  
+
   //test filter by language
 
   // const getLanguages= async (req, res)=>{Book.aggregate([{$group: {_id: "$language"}}]); }
