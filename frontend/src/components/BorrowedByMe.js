58302a6ff751a474a56ee39d34a0cd07e282dac2
Roxana Bita <roxana.bita@gmail.com>
2023-01-16 23:01:36 +0100

    separate components borrowed and lent books

---
+++ frontend/src/components/BorrowedByMe.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useNavigate } from "react-router";
+
+function BorrowedByMe({books}) {
+    const navigate = useNavigate();
+    return (
+        <div>
+            <p>Books borrowed by me</p>
+            {books.map((b) => (
+                <div key={b._id}>
+                    <div>{b.title}</div>
+                    <div>Rented until {b.btime}</div>
+                    <div>Owner: {b.owner}</div>{" "}
+                    <button
+                        onClick={() => navigate(`/catalogue/${b.book_id}`)}
+                        className=' bg-white bg-opacity-60 px-6 py-2 border-2 border-white-500  font-medium text-xs leading-tight  rounded-full hover:bg-pink-600 hover:bg-opacity-[45%] focus:outline-none focus:ring-0 transition duration-150 ease-in-out cursor:pointer'>
+                        More Info
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default BorrowedByMe;
